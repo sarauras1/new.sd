a1ca38912cbc0473951c5d0a834819184ba455d9
sarauras1 <41049600+sarauras1@users.noreply.github.com>
2019-11-08 22:47:54 +0000

    all files

---
+++ resources/scripts/main.js
@@ -0,0 +1,31 @@
+
+
+$(document).ready(function(){
+  $(".button").click(function(){
+    $(".content").hide();
+   
+  });
+  $("#collush").click(function(){
+    $("#lush").show();
+  });
+
+$("#colbrows").click(function(){
+  $("#brows").show();
+});
+
+$("#colthred").click(function(){
+  $("#thred").show();
+});
+$("#colwaxing").click(function(){
+  $("#waxing").show();
+});
+$("#coltint").click(function(){
+  $("#tint").show();
+});
+$("#colmicro").click(function(){
+  $("#micro").show();
+});
+$("#lushTint").click(function(){
+  $("#lushT").show();
+});
+});
